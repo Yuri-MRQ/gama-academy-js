baf2138be1d2d098d61fb9a1a85af1ebdc1d5c0e
Yuri Macedo Rezende Quintanilha <ymquint@gmail.com>
2021-07-05 20:01:52 -0300

    add remove functionality

--- learning-js.js
+++ learning-js.js
@@ -15,20 +15,47 @@ listForm.addEventListener('submit', function(e){
     CleanList()
 })
 
+
 function createList(name){
     return {id: Date.now().toString, name: name}
 }
 
+let counter = 0
 function render() {
     lists.forEach(function(list){
         const item = document.createElement('li')
-        item.classList.add('item')
+        const remove_item = document.createElement('button')
+
+        // create new item
+        item.classList.add('item' + counter.toString())
+        item.id = 'new-item' + counter.toString()
+        // create new button to remove
+        remove_item.classList.add('remove')
+        remove_item.id = counter.toString()
+
+        // add the text
+        remove_item.innerText = '-'
         item.innerText = list.name
+
         listContainer.appendChild(item)
+        item.appendChild(remove_item)
+
+        counter ++
+
         
     })
 }
 
+document.addEventListener('click', function(e){
+    itemRemove = document.getElementById('new-item' + e.target.id)
+    if (e.target.id != 'input' && e.target.id != 'add'){
+        listContainer.removeChild(itemRemove)
+    } 
+})
+
+
+
 function CleanList(){
     lists = []
 }
+
